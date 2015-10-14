77a1ee2c198a39d4ca1f8ad580fa46106f417dd3
yevgenryeznik <yevgen.ryeznik@gmail.com>
2015-10-14 16:10:17 +0200

    ggplot part is added

--- Lab2a.R
+++ Lab2a.R
@@ -1,4 +1,4 @@
-# optimization part -------------------------------------------------
+## optimization part -------------------------------------------------
 ofv <- function(x){ # function to be optimized
   (x - 3)^2 + 2*(x - 3)^2 + 3 * (x - 15)^2 + sin(100*x)
 }
@@ -11,7 +11,7 @@ do.opt <- function(interval, fun = ofv){ # optimization
 
 lapply(list(c(0, 100), c(0, 15), c(9, 12), c(10, 11)), do.opt)
 
-# integration part --------------------------------------------------
+## integration part --------------------------------------------------
 int.compareness <- function(fun, left, right, subdiv = 1e7, subint = 15){ # compare different approaches
   do.int <- function(interval, fcn = fun){ # integration
     return(integrate(fcn, lower = interval[1], upper = interval[2], subdivisions = subdiv)$value)
@@ -47,4 +47,24 @@ int.compareness(function(x){x*sin(x)}, left = -7e5, right = 7e5)
 
 # test for my example: exp(x^2)
 int.compareness(function(x){sin(x^2)}, left = 0, right = 500, subint = 50)
+
+## memoisation part ---------------------------------------------------------
+fib <- function(n) {
+  if (n < 2) return(1)
+  fib(n - 2) + fib(n - 1)
+}
+
+fib2 <- memoise(function(n) {
+  if (n < 2) return(1)
+  fib2(n - 2) + fib2(n - 1)
+})
+
+fib3 <- memoise(fib)
+
+## ggplot2 part -----------------------------------------------------
+qplot(carat, price, data = diamonds)
+qplot(carat, price, data = diamonds, color = color)
+diam.plt <- qplot(carat, price, data = diamonds, color = color, geom = c("point", "smooth")) 
+diam.plt + scale_colour_brewer(name = "New Legend")
+qplot(carat, price, data = diamonds, color = color, geom = "boxplot")
 
