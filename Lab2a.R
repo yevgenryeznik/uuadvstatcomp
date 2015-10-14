3cca3ef7a7cff43a8436cb300317b512173dbda8
yevgenryeznik <yevgen.ryeznik@gmail.com>
2015-10-14 14:06:38 +0200

    Optimization procedure is added.

--- Lab2a.R
+++ Lab2a.R
@@ -2,4 +2,10 @@ ofv <- function(x){
   (x - 3)^2 + 2*(x - 3)^2 + 3 * (x - 15)^2 + sin(100*x)
 }
 
+do.opt <- function(interval, fun = ofv){
+  sol <- optimise(ofv, interval)
+  out <- sprintf("x.min = %f, ofv = %f", sol$minimum, sol$objective)
+  return(out)
+}
+
 
