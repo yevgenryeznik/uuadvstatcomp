d4723b46c9090d1806f31567b2573837dca376d9
yevgenryeznik <yevgen.ryeznik@gmail.com>
2015-10-14 14:12:36 +0200

    comment is added

--- Lab2a.R
+++ Lab2a.R
@@ -1,12 +1,14 @@
-ofv <- function(x){
+# optimization part -------------------------------------------------
+ofv <- function(x){ # function to be optimized
   (x - 3)^2 + 2*(x - 3)^2 + 3 * (x - 15)^2 + sin(100*x)
 }
 
-do.opt <- function(interval, fun = ofv){
-  sol <- optimise(ofv, interval)
+do.opt <- function(interval, fun = ofv){ # optimization 
+  sol <- optimise(fun, interval)
   out <- sprintf("x.min = %f, ofv = %f", sol$minimum, sol$objective)
   return(out)
 }
 
 lapply(list(c(0, 100), c(0, 15), c(9, 12), c(10, 11)), do.opt)
 
+# integration part --------------------------------------------------
